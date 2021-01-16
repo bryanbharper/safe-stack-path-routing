88b9bed0df7b92c46ad7d354b8c63699eb174c1a
Bryan Harper <brharper@corelogic.com>
2021-01-16 16:24:17 -0600

    Cleanup.

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,12 +1,10 @@
 module Client.Index
 
+(*******************************************
+*               TYPES
+*******************************************)
 open Client.Pages
-
-type Url =
-    | About
-    | Blog
-    | BlogEntry of string
-    | Todo
+open Client.Urls
 
 type Page =
     | About
@@ -23,6 +21,9 @@ type Msg =
     | Todo of Todo.Msg
 
 
+(*******************************************
+*               INIT
+*******************************************)
 open Elmish
 
 let initFromUrl url =
@@ -42,6 +43,10 @@ let init (): State * Cmd<Msg> =
     let state, cmd = Blog.init ()
     { CurrentPage = Page.Blog state }, Cmd.map Msg.Blog cmd
 
+
+(*******************************************
+*               UPDATE
+*******************************************)
 let update (msg: Msg) (state: State): State * Cmd<Msg> =
     match msg, state.CurrentPage with
     | UrlChanged url, _ -> initFromUrl url
@@ -63,6 +68,9 @@ let update (msg: Msg) (state: State): State * Cmd<Msg> =
     | _ -> state, Cmd.none
 
 
+(*******************************************
+*               RENDER
+*******************************************)
 open Feliz
 open Feliz.Bulma
 
@@ -72,7 +80,7 @@ let navbar dispatch =
         prop.children [
             Bulma.navbarBrand.div [
                 Bulma.navbarItem.a [
-                    prop.href ""
+                    prop.onClick (fun _ -> Url.Blog |> Msg.UrlChanged |> dispatch)
                     prop.children [
                         Html.img [ prop.src "favicon.png" ]
                     ]
@@ -81,15 +89,15 @@ let navbar dispatch =
             Bulma.navbarMenu [
                 Bulma.navbarEnd.div [
                     Bulma.navbarItem.a [
-                        prop.text "about"
+                        prop.text Url.About.asString
                         prop.onClick (fun _ -> Url.About |> Msg.UrlChanged |> dispatch)
                     ]
                     Bulma.navbarItem.a [
-                        prop.text "blog"
+                        prop.text Url.Blog.asString
                         prop.onClick (fun _ -> Url.Blog |> Msg.UrlChanged |> dispatch)
                     ]
                     Bulma.navbarItem.a [
-                        prop.text "todo"
+                        prop.text Url.Todo.asString
                         prop.onClick (fun _ -> Url.Todo |> Msg.UrlChanged |> dispatch)
                     ]
                 ]
---
+++ src/Client/Urls.fs
@@ -0,0 +1,14 @@
+﻿module Client.Urls
+
+open Microsoft.FSharp.Reflection
+
+type Url =
+    | About
+    | Blog
+    | BlogEntry of string
+    | Todo
+    member this.asString =
+        let (case, _) =
+            FSharpValue.GetUnionFields(this, typeof<Url>)
+
+        case.Name.ToLower()
