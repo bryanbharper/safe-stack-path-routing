2ad1ffcdb2a53787a64364fb01c5839f6bfac554
Bryan Harper <brharper@corelogic.com>
2021-01-17 11:25:52 -0600

    Put helper in module.

--- src/Client/Urls.fs
+++ src/Client/Urls.fs
@@ -14,16 +14,17 @@ type Url =
 
         case.Name.ToLower()
 
-let fromString (s: string) =
-    let caseInfo =
-        FSharpType.GetUnionCases typeof<Url>
-        |> Array.tryFind (fun case -> case.Name.ToLower() = s.ToLower())
+    module Url =
+        let fromString (s: string) =
+            let caseInfo =
+                FSharpType.GetUnionCases typeof<Url>
+                |> Array.tryFind (fun case -> case.Name.ToLower() = s.ToLower())
 
-    match caseInfo with
-    | Some case ->
-        match case.GetFields() with
-        | [||] ->
-            FSharpValue.MakeUnion(case, [||]) :?> Url |> Some
-        | _ ->
-            FSharpValue.MakeUnion(case,  [| "" |> box |] ) :?> Url |> Some
-    | _ -> None
+            match caseInfo with
+            | Some case ->
+                match case.GetFields() with
+                | [||] ->
+                    FSharpValue.MakeUnion(case, [||]) :?> Url |> Some
+                | _ ->
+                    FSharpValue.MakeUnion(case,  [| "" |> box |] ) :?> Url |> Some
+            | _ -> None
